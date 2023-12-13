12d3d3ed07a20cda916017b52fb322448899bec0
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-12-13 01:35:25 +0100

    add test schema model

---
+++ bond-backend/models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+//lets create a schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  subscribedToChannel: {
+    type: String,
+    required: true,
+  },
+  subscribeDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
