45019d945ead63f1e70aa46880488e1e9efef1e7
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-12-17 13:05:52 +0100

    user folder structure

---
+++ bond-backend/services/user.services.js
@@ -0,0 +1,27 @@
+const Users = require("../models/user.model");
+const BaseService = require("./base.service");
+
+class UserService extends BaseService {
+  constructor(model) {
+    super(model);
+  }
+
+  async find(filter) {
+    return this.model.findOne(filter, "-posts -replies -password -deleted");
+  }
+
+  async findWithDetails(filter) {
+    return await this.model.findOne(filter);
+  }
+
+  async findAll(filter) {
+    return await this.model.find(filter, "-posts -replies -password -deleted");
+  }
+
+  async updateOne(filter, data) {
+    return await this.model.updateOne(filter, { $push: { posts: data } });
+  }
+}
+
+const user = new UserService(Users);
+module.exports = user;
