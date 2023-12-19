469b04a257109360ac1186483e0c581d5713016d
“sitoaustin” <onyedibesixtusna@gmail.com>
2023-12-19 22:13:32 +0100

    Feature: added mobile view profile page

--- bond-frontend/app/(social)/social/page.tsx
+++ bond-frontend/app/(social)/social/page.tsx
@@ -4,6 +4,7 @@ import Right_panel_dashboard from './social-components/Right-panel-dashboard';
 export default function page() {
   return (
     <section className='w-full bg-primary md:col-span-5 max-h-[100vh] md:grid md:grid-rows-[(10vh_1fr)]'>
+      {/* <section className='w-full bg-primary md:col-span-5 md:grid md:grid-rows-[(10vh_1fr)]'> */}
       <Right_panel_dashboard />
     </section>
   );
