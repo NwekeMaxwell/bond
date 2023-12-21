acbf9399ab3f9d193d64da55d458ddf392a4eff2
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-12-21 16:18:19 +0100

    update joi services

--- bond-backend/services/joi.service.js
+++ bond-backend/services/joi.service.js
@@ -2,10 +2,15 @@ const Joi = require("joi");
 
 //check the user schema fields against pre-set conditions
 const userSchema = Joi.object({
-  fullName: Joi.string().required,
+  firstname: Joi.string().required(),
+  lastname: Joi.string().required(),
   username: Joi.string().required().lowercase(),
   email: Joi.string().email().required(),
   password: Joi.string().min(8).max(30).required(),
+  avatar: Joi.string(),
+  role: Joi.string(),
+  phone: Joi.string(),
+  about: Joi.string(),
 });
 
 // Checking the postit schema fields against pre-set conditions
