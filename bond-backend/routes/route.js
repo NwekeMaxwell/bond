269e92e3dc3ef656b049d2bae0b00b99adbb30e2
NwekeMaxwell <nwekemaxwell37@gmail.com>
2023-12-22 12:08:11 +0100

    add comment route

--- bond-backend/routes/route.js
+++ bond-backend/routes/route.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const userRouter = require("./user.route");
 const authRouter = require("./auth.route");
 const postsRouter = require("./posts.route");
+const commentRouter = require("./comment.route");
 
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/posts", postsRouter);
+router.use("/comment", commentRouter);
 
 module.exports = router;
