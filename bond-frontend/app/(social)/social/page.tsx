5288d686d5cc671d252c977d5c0b3482f62ba24e
“sitoaustin” <onyedibesixtusna@gmail.com>
2024-01-16 23:03:51 +0100

    Refactor: changed file name

--- bond-frontend/app/(social)/social/page.tsx
+++ bond-frontend/app/(social)/social/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import Profile_Right_panel_dashboard from './social-components/Home_Right_panel_dashboard';
+import Home_Right_panel_dashboard from './components/Home_Right_panel_dashboard';
 export default function page() {
   return (
     <section className='w-full bg-primary md:col-span-3 max-h-[100vh] md:grid md:grid-rows-[(10vh_1fr)]'>
-      <Profile_Right_panel_dashboard />
+      <Home_Right_panel_dashboard />
     </section>
   );
 }
