c2f17f1f223c39feb0c9262af06f795521a0d163
“sitoaustin” <onyedibesixtusna@gmail.com>
2024-01-16 23:07:18 +0100

    Refactor: added types file for user data

--- bond-frontend/app/(social)/social/Types/UserDataTypes.ts
+++ bond-frontend/app/(social)/social/Types/UserDataTypes.ts
@@ -8,3 +8,29 @@ export type UserData = {
   _v: number;
   _id: string;
 };
+
+export type UsersPost = {
+  _id: string;
+  author: {
+    username: string;
+    _id: string;
+  };
+  content: string;
+  parentpost: {
+    _id: string;
+  };
+  image1: string;
+};
+
+export type Users_Data_from_db = {
+  author: { _id: string; username: string };
+  content: string;
+  createdAt: string;
+  image1: string;
+  image2?: string;
+  image3?: string;
+  image4?: string;
+  updatedAt: string;
+  _v: number;
+  _id: string;
+};
