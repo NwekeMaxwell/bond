e5a4d82177d5fb279f987a88f2e54340692927d2
“sitoaustin” <onyedibesixtusna@gmail.com>
2024-01-16 13:18:39 +0100

    Refactor: added the types to Types folder and made checklogged in user resuable

---
+++ bond-frontend/app/(social)/social/Types/UserDataTypes.ts
@@ -0,0 +1,10 @@
+export type UserData = {
+  createdAt: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  updatedAt: string;
+  username: string;
+  _v: number;
+  _id: string;
+};
